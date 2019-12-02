3d53a6c81a02813e5b0ca0debee4f16337b78f73
chenzihang <1164734537@qq.com>
2019-12-02 17:52:24 +0800

    增加数据mock

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,6 +9,17 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+// 第一步
+// 使用node express 框架
+const express = require('express')
+const app = express() // 实例化
+var appData = require('../data.json')// 加载本地数据文件
+var seller = appData.seller // 获取对应的本地数据
+var goods = appData.goods
+var ratings = appData.ratings
+var apiRoutes = express.Router()
+app.use('/api', apiRoutes)  // 通过路由请求数据
+
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -42,7 +53,37 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
-    }
+    },
+      // 第二步,在devServer,在里面添加
+      before(app) {
+        app.get('/api/appData', (req, res) => {
+          res.json({
+            errno: 0,
+            data: appData
+          })//接口返回json数据，上面配置的数据appData就赋值给data请求后调用
+        }),
+        app.get('/api/seller', (req, res) => {
+            res.json({
+              // 这里是你的json内容
+              errno: 0,
+              data: seller
+            })
+          }),
+        app.get('/api/goods', (req, res) => {
+            res.json({
+              // 这里是你的json内容
+              errno: 0,
+              data: goods
+            })
+          }),
+        app.get('/api/ratings', (req, res) => {
+            res.json({
+              // 这里是你的json内容
+              errno: 0,
+              data: ratings
+          })
+        })
+     }
   },
   plugins: [
     new webpack.DefinePlugin({
