0bf6fe67b33ee3a1b007d8aa75e98ed5969f61c7
chenzihang <1164734537@qq.com>
2019-12-04 18:00:58 +0800

    增加组件,引入 stylus

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
+import Vue from 'vue';
+import Router from 'vue-router';
+// import HelloWorld from '@/components/HelloWorld'
+import goods from '@/components/goods/goods.vue';
+import ratings from '@/components/ratings/ratings.vue';
+import seller from '@/components/seller/seller.vue';
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '/goods',
+      name: 'goods',
+      component: goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: seller
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/goods',
+      component: goods
     }
-  ]
-})
+  ],
+  linkActiveClass: 'active' // active为路由激活时动态添加的类，类名可以自定义
+});
