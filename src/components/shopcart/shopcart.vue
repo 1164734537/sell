eb34f95b9b44b334304d89d192d0754af7f3e90e
chenzihang <1164734537@qq.com>
2020-01-04 14:34:38 +0800

    为购物车设计selectFoods监听属性,动态改变样式

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,16 +3,18 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{ totalCount }}</div>
         </div>
-        <div class="price">&#165;0元</div>
+        <div class="price" :class="{'highlight':totalPrice>0}">&#165;{{ totalPrice }}元</div>
         <div class="desc">另需配送费 &#165;{{ deliveryPrice }}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          &#165;{{ minPrice }}元起送
+        <div class="pay" :class="payClass">
+         <!-- &#165;{{ minPrice }}元起送 -->
+          {{ payDesc }}
         </div>
       </div>
     </div>
@@ -22,6 +24,17 @@
 <script>
 export default {
   props: {
+    selectFoods: {
+      type: Array,
+      default() {
+        return [
+          {
+            price: 10,
+            count: 1
+          }
+        ];
+      }
+    },
     deliveryPrice: {
       type: Number,
       default: 0
@@ -29,6 +42,39 @@ export default {
     minPrice: {
       type: Number,
       default: 0
+    }
+  },
+  computed: {
+    totalPrice() {
+      let total = 0;
+      this.selectFoods.forEach((food) => {
+        total += food.price * food.count;
+      });
+      return total;
+    },
+    totalCount() {
+      let count = 0;
+      this.selectFoods.forEach((food) => {
+        count += food.count;
+      });
+      return count;
+    },
+    payDesc() {
+      if (this.totalPrice === 0) {
+        return `¥ ${this.minPrice}元起送`;
+      } else if (this.totalPrice < this.minPrice) {
+        let diff = this.minPrice - this.totalPrice;
+        return `¥ 还差${diff}元起送`;
+      } else {
+        return '去结算';
+      }
+    },
+    payClass() {
+      if (this.totalPrice < this.minPrice) {
+        return 'not-enought';
+      } else {
+        return 'enought';
+      }
     }
   }
 };
@@ -69,10 +115,28 @@ export default {
             border-radius: 50%
             background: #2b343c
             text-align:center
+            &.highlight
+              background:rgb(0,160,220)
             .icon-shopping_cart
               line-height:44px
               font-size:24px
               color: #80858a
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            background: rgb(240,20,20)
+            color: #fff
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,.4)
         .price
           display:inline-block
           vertical-align: top
@@ -83,6 +147,8 @@ export default {
           border-right: 1px solid rgba(255,255,255,.1)
           font-size: 16px
           font-weight: 700
+          &.highlight
+            color: #fff
         .desc
           display:inline-block
           vertical-align: top
@@ -99,6 +165,9 @@ export default {
           text-align: center
           font-size: 12px
           font-weight: 700
-          background: #2b333b
-
+          &.not-enought
+            background: #2b333b
+          &.enought
+            background: #00b43c
+            color: #fff
 </style>
